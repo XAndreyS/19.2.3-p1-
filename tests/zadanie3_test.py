e86cf82b85c2e94d2920b34f7f0d5361887b588d
XAndreyS <archer.wipt@gmail.com>
2022-02-06 02:50:42 +0500

    version 1.0

---
+++ tests/zadanie3_test.py
@@ -0,0 +1,19 @@
+import pytest
+from app.calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+
+    def test_multiply_calculate_corrrectly(self):
+        assert self.calc.multiply(self, 22, 22) == 484
+
+    def test_division_correctly(self):
+        assert self.calc.division(self, 21, 3) == 7
+
+    def test_subtraction_correctly(self):
+        assert self.calc.subtraction(self, 20, 10) == 10
+
+    def test_adding_correctly(self):
+        assert self.calc.adding(self, 100, 100) == 200
